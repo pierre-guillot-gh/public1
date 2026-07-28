88833fd5a32245095123f5da18b73a70f9d0b169
Pierre <pierre.guillot@sonarsource.com>
2026-07-28 15:46:58 +0200

    a big more bugs

--- file.py
+++ file.py
@@ -1,16 +1,4 @@
-class Resource:
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc, tb):
-        ...
-
-    async def __aenter__(self):
-        return self
-
-    async def __aexit__(self, exc_type, exc, tb):
-        ...
-
-async def main():
-    resource = Resource()
-    with resource:  # Noncompliant: using 'with' in async function when async protocol is available
+class MyClass(object):
+    def __init__(self):
+        self.message = 'Hello'
+        return self  # Noncompliant: a TypeError will be raised
